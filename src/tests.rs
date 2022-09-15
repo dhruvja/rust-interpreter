82506c1754ee1f3422cd2b7f24db4ea569ddb293
dhruvja <dhruv@iamsizzling.com>
2022-09-15 17:57:30 +0530

    added a test to do perform multiple operations

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,5 @@
-
-
 #[cfg(test)]
 mod interpreter_tests {
-
 
     // use super::*;
     use crate::bytecode_types::{ByteCode::*, CodeError};
@@ -18,12 +15,10 @@ mod interpreter_tests {
 
     #[test]
     fn test_value_loaded() {
-
         let x = 7;
         let instructions = vec![LoadVal(x), Return];
 
         assert_eq!(interpret(instructions).unwrap().value, x);
-
     }
 
     #[test]
@@ -32,45 +27,44 @@ mod interpreter_tests {
 
         let instructions = vec![LoadVal(x), Add, Return];
 
-        assert_eq!(interpret(instructions).err(), Some(CodeError::StackUnderflow));
+        assert_eq!(
+            interpret(instructions).err(),
+            Some(CodeError::StackUnderflow)
+        );
     }
 
     #[test]
     fn test_addition() {
-
         let x = 2;
         let y = 4;
-        let sum = x+y;
+        let sum = x + y;
         let instructions = vec![LoadVal(x), LoadVal(y), Add, Return];
 
         assert_eq!(interpret(instructions).unwrap().value, sum);
     }
     #[test]
     fn test_subtraction() {
-
         let x = 2;
         let y = 4;
-        let diff = x-y;
+        let diff = x - y;
         let instructions = vec![LoadVal(x), LoadVal(y), Sub, Return];
 
         assert_eq!(interpret(instructions).unwrap().value, diff);
     }
     #[test]
     fn test_multiplication() {
-
         let x = 2;
         let y = 4;
-        let product = x*y;
+        let product = x * y;
         let instructions = vec![LoadVal(x), LoadVal(y), Mul, Return];
 
         assert_eq!(interpret(instructions).unwrap().value, product);
     }
     #[test]
     fn test_division() {
-
         let x = 2;
         let y = 4;
-        let quotient = x/y;
+        let quotient = x / y;
         let instructions = vec![LoadVal(x), LoadVal(y), Div, Return];
 
         assert_eq!(interpret(instructions).unwrap().value, quotient);
@@ -79,10 +73,31 @@ mod interpreter_tests {
     fn test_mod() {
         let x = 2;
         let y = 4;
-        let remainder = x%y;
+        let remainder = x % y;
         let instructions = vec![LoadVal(x), LoadVal(y), Mod, Return];
 
         assert_eq!(interpret(instructions).unwrap().value, remainder);
     }
 
+    #[test]
+    fn test_multiple_operations() {
+        let x = 2;
+        let y = 4;
+        let z = 6;
+
+        let result = (x * y) + z - x;
+
+        let instructions = vec![
+            LoadVal(x),
+            LoadVal(y),
+            Mul,
+            LoadVal(z),
+            Add,
+            LoadVal(x),
+            Sub,
+            Return,
+        ];
+
+        assert_eq!(interpret(instructions).unwrap().value, result);
+    }
 }
